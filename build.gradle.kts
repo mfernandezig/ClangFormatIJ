4facbfa4309f04aab4bfe5a034c1e2ffb06fc5ec
Miguel Angelo Fernández Iglesias <82086544+mfernandezig@users.noreply.github.com>
2023-07-04 17:36:29 +0200

    Sourcen und Lombok ergänzt

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,9 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
+dependencies {
+    implementation("org.projectlombok:lombok:1.18.26")
+}
 
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
